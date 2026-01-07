2c10b0ce91a18e567438d78fba24b30c651932fd
Teo Tanaka <teo.tanaka777@gmail.com>
2026-01-07 11:03:14 -0800

    comments and this. on variables

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 //file:///C:/Users/teo.tanaka27/stickfight/Stick-Fight/index.html
+//git config --global user.email "teo.tanaka777@gmail.com" 
+//git config --global user.name "Teo Tanaka" 
 let p = new Point(250,250);
 function setup(){
     createCanvas(500,500)
